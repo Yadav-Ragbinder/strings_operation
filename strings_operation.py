1bab996674260e4e0c25ea72222f4bfaf9388c0c
Yadav-Ragbinder <123340685+Yadav-Ragbinder@users.noreply.github.com>
2023-01-22 22:19:35 -0500

    Add files via upload

---
+++ strings_operation.py
@@ -0,0 +1,20 @@
+x = "Pratibha Singh"
+#lower case
+print (x.lower())
+#upper case
+print (x.upper())
+#range 
+print (x[:6])
+print (x[:5])
+#index
+print (x[4])
+print (x.index("b"))
+#replace
+print (x.replace("Pratibha","Kohli"))
+print (x.replace("i","k"))
+#find
+x1 = ( x + "i")
+print (x1.replace("i","k"))
+#add two str
+y = " Kohli"
+print (x + y)
