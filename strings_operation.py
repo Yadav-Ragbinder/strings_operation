9ede0c62207dd3fa71359472cf025b0d12aab8c4
Ragbinder Yadav <123340685+Yadav-Ragbinder@users.noreply.github.com>
2023-01-25 05:14:02 -0600

    Update strings_operation.py

--- strings_operation.py
+++ strings_operation.py
@@ -1,20 +1,63 @@
+// changes by developer Ragbinder Yadav
 x = "Pratibha Singh"
 #lower case
 print (x.lower())
+-----------------------------------
 #upper case
 print (x.upper())
+-----------------------------------
 #range 
 print (x[:6])
 print (x[:5])
+
+-----------------------------------
 #index
 print (x[4])
 print (x.index("b"))
+
+-----------------------------------
 #replace
 print (x.replace("Pratibha","Kohli"))
 print (x.replace("i","k"))
+
+-----------------------------------
 #find
 x1 = ( x + "i")
 print (x1.replace("i","k"))
+
+-----------------------------------
 #add two str
 y = " Kohli"
 print (x + y)
+
+// changes by developer Pratibha kohli
+
+// variables in string
+name = "hello world"
+print(f"hi {name}")
+# hi hello world
+-------------------------------------
+// Print a float with a specified number of digits:
+pi = 3.1415926535897932384 
+print(f"The first 5 digits of pi are {pi:.5f}")
+# The first 5 digits of pi are 3.14159
+
+--------------------------------------
+// In Place Boolean Logic
+new_user = False
+user_name = "Alexander Hamilton"
+print(f"{'Congrats on making your account' if new_user else 'Welcome back'} {user_name}!")
+# Welcome back Alexander Hamilton!
+
+------------------------------------
+
+// Python String strip() Method
+
+txt = "     banana     "
+x = txt.strip()
+print("of all fruits", x, "is my favorite") 
+# of all fruits banana is my favorite 
+
+----------------------------------------------
+
+// End of commit changes by Developer Pratibha Kohli
